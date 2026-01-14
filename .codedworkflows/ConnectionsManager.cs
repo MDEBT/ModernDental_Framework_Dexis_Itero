6de480f670d1807241c939d57967ef23d31148c1
ModernDental_Framework <ict@elysee-dental.nl>
2026-01-14 23:34:08 +0100

    changes ind dexis

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace ModernDental_Framework__Dexis_Itero
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
